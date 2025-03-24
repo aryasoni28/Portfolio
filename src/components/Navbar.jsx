2c5f458faea0fad3def1548a106abdbbce9dfc24
aryasoni28 <147711984+aryasoni28@users.noreply.github.com>
2025-03-24 23:32:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
           
         </Link>
         <a
-    href="/Arya_Soni_Resume.pdf" // Path to your resume file in the public folder
+    href="/Arya_Soni_Resume.pdf.pdf" // Path to your resume file in the public folder
     download="Arya_Soni_Resume.pdf"
     className="bg-blue-600 text-white px-4 py-2 rounded-lg font-medium hover:bg-blue-700 transition duration-300"
   >
